73c67af56dee1134b0ffff7f9c96308c0f8dbd20
EdwinSaucedoMX <edwinsaucedomx@gmail.com>
2024-04-14 12:01:26 -0600

    fix(financial): fix color string undefined type error

--- financial-house-app/src/app/profile/[id]/page.tsx
+++ financial-house-app/src/app/profile/[id]/page.tsx
@@ -47,17 +47,17 @@ export default function Page({ params }: { params: { id: string } }) {
             viewBoxSize={[100, 100]}
           /> */}
           <FullOption data={[
-            { title: 'One', value: 10, color: colors.at(0) },
-            { title: 'Two', value: 15, color: colors.at(1) },
-            { title: 'Three', value: 20, color: colors.at(2) },
+            { title: 'One', value: 10, color: colors.at(0) ?? "rgb( 109,40,217)" },
+            { title: 'Two', value: 15, color: colors.at(1) ?? "rgb( 109,40,217)" },
+            { title: 'Three', value: 20, color: colors.at(2) ?? "rgb( 109,40,217)" },
             // Repeat pattern counting to Ten
-            { title: 'Four', value: 25, color: colors.at(3) },
-            { title: 'Five', value: 30, color: colors.at(4) },
-            { title: 'Six', value: 35, color: colors.at(5) },
-            { title: 'Seven', value: 40, color: colors.at(6) },
-            { title: 'Eight', value: 45, color: colors.at(7) },
-            { title: 'Nine', value: 50, color: colors.at(8) },
-            { title: 'Ten', value: 55, color: colors.at(9) },
+            { title: 'Four', value: 25, color: colors.at(3) ?? "rgb( 109,40,217)" },
+            { title: 'Five', value: 30, color: colors.at(4) ?? "rgb( 109,40,217)" },
+            { title: 'Six', value: 35, color: colors.at(5) ?? "rgb( 109,40,217)" },
+            { title: 'Seven', value: 40, color: colors.at(6) ?? "rgb( 109,40,217)" },
+            { title: 'Eight', value: 45, color: colors.at(7) ?? "rgb( 109,40,217)" },
+            { title: 'Nine', value: 50, color: colors.at(8) ?? "rgb( 109,40,217)" },
+            { title: 'Ten', value: 55, color: colors.at(9) ?? "rgb( 109,40,217)" },
           ]}
             className="grow aspect-square  h-64" />
         </section>
