d8f2c964efdab2425ff543b335f3bbaa49d70c0a
EdwinSaucedoMX <edwinsaucedomx@gmail.com>
2024-04-19 19:01:00 -0600

    fix(financial):fix routing on aside

--- financial-house-app/src/app/profile/layout.tsx
+++ financial-house-app/src/app/profile/layout.tsx
@@ -1,13 +1,37 @@
 "use client"
-import { PersonStanding, PieChart, TrendingUp } from "lucide-react"
+import { PersonStanding, PieChart, TrendingUp, DoorClosed } from "lucide-react"
 import { BurgerMenu } from "./client-components"
 import { AsideSection, ProfileCard } from "./components"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import Link from "next/link"
+import { usePathname, useParams } from "next/navigation"
 
 export default function Layout(props: { children: React.ReactNode }) {
-  const [active, setActive] = useState(0)
+  const route = usePathname().split("/")
+  const section = route.at(2)
+  const pathname = route.at(1)
+
+  const getActive = () => {
+    switch (section) {
+      case "movements":
+        return 1
+        break
+      case "family":
+        return 2
+        break
+      default:
+        return 0
+        break
+    }
+  }
+
+  const [active, setActive] = useState(getActive())
 
   const handleActive = (index: number) => setActive(index)
+
+  const { id } = useParams()
+  /* const id = pathname.split("/").at(-2)
+  const current = pathname.split("/").at(-2) */
 
   const getClassActive = (index: number) => {
     if (active === index) {
@@ -15,6 +39,8 @@ export default function Layout(props: { children: React.ReactNode }) {
     }
     return ""
   }
+
+
   return (
     <main className="flex flex-row relative h-dvh ">
 
@@ -24,11 +50,21 @@ export default function Layout(props: { children: React.ReactNode }) {
       <aside id="aside" className="hidden sm:block  bg-secondary sm:m-2 box-border sm:rounded-lg  overflow-hidden sm:min-w-16 sm:hover:min-w-64 min-w-full transition-all group/aside  target:block relative">
 
         <ProfileCard />
-        <AsideSection icon={PieChart} title="Dashboard" activeclass={getClassActive(0)} onClick={() => handleActive(0)} />
-        <AsideSection icon={TrendingUp} title="Movimientos" activeclass={getClassActive(1)} onClick={() => handleActive(1)} />
-        <AsideSection icon={PersonStanding} title="Familia" activeclass={getClassActive(2)} onClick={() => handleActive(2)} />
+        <Link href={`/${pathname}/dashboard/${id}`}>
+          <AsideSection icon={PieChart} title="Dashboard" activeclass={getClassActive(0)} onClick={() => handleActive(0)} />
+        </Link>
+        <Link href={`/${pathname}/movements/${id}`}>
+          <AsideSection icon={TrendingUp} title="Movimientos" activeclass={getClassActive(1)} onClick={() => handleActive(1)} />
+        </Link>
+        <Link href={`/${pathname}/family`}>
+          <AsideSection icon={PersonStanding} title="Familia" activeclass={getClassActive(2)} onClick={() => handleActive(2)} />
+        </Link>
+        <Link href="/" className="absolute bottom-0 w-full">
+          <AsideSection icon={DoorClosed} title="Cerrar Sesion" activeclass={getClassActive(2)} onClick={() => handleActive(2)} />
+        </Link>
 
         {/* <Link href="/profile" className="absolute bottom-8">Cerrar Sesion</Link> */}
+
       </aside>
       {props.children}
     </main >
--- financial-house-app/src/app/profile/movements/[id]/page.tsx
+++ financial-house-app/src/app/profile/movements/[id]/page.tsx
@@ -1,7 +1,9 @@
 export default function UserMovementsPage(props: any) {
   return (
-    <div>
-      <h1>User Movements Page</h1>
-    </div>
+    <section className="w-full flex flex-wrap gap-2  sm:py-0 py-12">
+      <section className="w-full flex flex-wrap gap-2 p-4 content-start ">
+        <h1>User Movements Page</h1>
+      </section>
+    </section>
   )
 }
